022fb2dfad2ddb8c9782adc2cee2e8e4540625f6
Brandon Van Pelt <bvanpelt@hypertech.com>
2021-09-29 10:37:12 -0500

    Current

--- PCAN_Reformat/PCAN_Reformat.cpp
+++ PCAN_Reformat/PCAN_Reformat.cpp
@@ -1,7 +1,7 @@
 /*
- Name:		PCAN_Reformat.ino
- Created:	6/22/2021 2:35:46 PM
- Author:	Brandon Van Pelt
+ Name:		  PCAN_Reformat.ino
+ Created:	  6/22/2021 2:35:46 PM
+ Author:	  Brandon Van Pelt
  Description: Formats data from multiple capture software to a single standard format
 */
 
@@ -23,9 +23,9 @@
 #define DEVMODE 
 
 // Pick a capture software
-#define PCAN_View
+//#define PCAN_View
 //#define ScanTool
-//#define PCAN_Explorer
+#define PCAN_Explorer
 
 // Pad frames shorter than 8 with zeros
 //#define PAD_ZERO
@@ -59,6 +59,8 @@ typedef struct
 } CANMSG;
 
 CANMSG buffer;
+
+int count = 0;
 
 // Convert capture to standard output text file
 void formatTRC(char* filename, char* fileOut)
@@ -107,7 +109,7 @@ void formatTRC(char* filename, char* fileOut)
         int messageNumber = 0;
         while (fscanf(ptr, "%f %x %d", &buffer.time, &buffer.id, &buffer.length) != EOF)
         {
-            buffer.messageNum = messageNumber++;
+            sprintf(buffer.messageNum, "  %06d", messageNumber++);
 #endif
 
 #if defined PCAN_Explorer
@@ -140,12 +142,12 @@ void formatTRC(char* filename, char* fileOut)
 #endif
 
         //printf("ID: %0x\n", buffer.id);
-        fprintf(fp, "% 7s   %9.3f   %04x   %d ", buffer.messageNum, buffer.time, buffer.id, buffer.length);
+        fprintf(fp, "%08s   %9.3f   %04x   %d ", buffer.messageNum, buffer.time, buffer.id, buffer.length);
 
 #if defined PAD_ZERO
         for (int i = 0; i < 8; i++)
         {
-            (i < buffer.length) ? (fscanf(ptr, "%x", &buffer.data[i])) : (buffer.data[i] = 0);
+            (i < 8) ? (fscanf(ptr, "%x", &buffer.data[i])) : (buffer.data[i] = 0);
             fprintf(fp, "  %02x", buffer.data[i]);
         }
 #else
@@ -162,30 +164,75 @@ void formatTRC(char* filename, char* fileOut)
     }
 }
 
-// TODO(Brandon) Convert to use with multiple capture software
-// Find all unique IDs and add them to list
+// Find onlys messages with passed ID
 void formatTRC(char* filename, char* fileOut, int id)
 {
+    char line_in[MAX_STRING_LENGTH];
 
     FILE* ptr = fopen(filename, "r");
     FILE* fp = fopen(fileOut, "w+");
 
     if (ptr == NULL)
     {
-        printf("no such file.");
+        printf("File not found!");
         return;
     }
 
-    char line_in[MAX_STRING_LENGTH];
-
+    // Remove the header from the file
     for (int i = 0; i < FILE_HEADER_SIZE; i++)
     {
         fgets(line_in, MAX_STRING_LENGTH, ptr);
     }
 
-    // Msg Num, Time, ID, Length
+#if defined PCAN_View
+    /*
+    ;-------------------------------------------------------------------------------
+    ;   Message   Time    Type ID     Rx/Tx
+    ;   Number    Offset  |    [hex]  |  Data Length
+    ;   |         [ms]    |    |      |  |  Data [hex] ...
+    ;   |         |       |    |      |  |  |
+    ;---+-- ------+------ +- --+----- +- +- +- +- -- -- -- -- -- -- --
+        1     13212.973  DT 18DB33F1 Rx  8  02 01 00 00 00 00 00 00
+    */
+    while (fscanf(ptr, "%s %f %*s %x %*s %d", &buffer.messageNum, &buffer.time, &buffer.id, &buffer.length) != EOF)
+    {
+#endif
+
+#if defined ScanTool
+    /*
+    ;-------------------------------------------------------------------------------
+    ;   Message   Time     ID
+    ;   Number    Offset   [hex]  Data Length
+    ;   |         [ms]     |      |  Data [hex] ...
+    ;   |         |        |      |  |
+    ;---+---------+--------+------+- +- -- -- -- -- -- -- --
+                00059172  0233     8  00 57 6F 72 6C 64 00 00
+    */
+    int messageNumber = 0;
+    while (fscanf(ptr, "%f %x %d", &buffer.time, &buffer.id, &buffer.length) != EOF)
+    {
+        sprintf(buffer.messageNum, "  %06d", messageNumber++);
+#endif
+
+#if defined PCAN_Explorer
+    /*
+    ;-------------------------------------------------------------------------------
+    ;   Message Number
+    ;   |         Time Offset (ms)
+    ;   |         |       Bus
+    ;   |         |       |    Type
+    ;   |         |       |    |       ID (hex)
+    ;   |         |       |    |       |    Reserved
+    ;   |         |       |    |       |    |   Data Length Code
+    ;   |         |       |    |       |    |   |    Data Bytes (hex) ...
+    ;   |         |       |    |       |    |   |    |
+    ;   |         |       |    |       |    |   |    |
+    ;---+-- ------+------ +- --+-- ----+--- +- -+-- -+ -- -- -- -- -- -- --
+        1)         0.279 1  Rx        018E -  8    00 00 00 64 16 42 07 82
+    */
     while (fscanf(ptr, "%s %f %*s %*s %x %*s %d", &buffer.messageNum, &buffer.time, &buffer.id, &buffer.length) != EOF)
     {
+        // Remove the ")" from the message number
         for (int i = 0; i < MAX_CHAR_LENGTH; i++)
         {
             if (buffer.messageNum[i] == ')')
@@ -194,24 +241,27 @@ void formatTRC(char* filename, char* fileOut, int id)
                 i = MAX_CHAR_LENGTH;
             }
         }
-        //printf("ID: %0x\n", buffer.id);
-        if (buffer.id == id)
+#endif
+        
+        //if (buffer.id == 0x42F || buffer.id == 0x326)
+        if (buffer.id == id || buffer.id == 0x7E8 || buffer.id == 0x101)
         {
-            fprintf(fp, "% 7s   %9.3f   %04x   %d ", buffer.messageNum, buffer.time, buffer.id, buffer.length);
+            fprintf(fp, "%08s   %9.3f   %04x   %d ", buffer.messageNum, buffer.time, buffer.id, buffer.length);
         }
 
         for (int i = 0; i < buffer.length; i++)
         {
             fscanf(ptr, "%x", &buffer.data[i]);
             //printf("%02x ", buffer.data[i]);
-
-            if (buffer.id == id)
+            if (buffer.id == id || buffer.id == 0x7E8 || buffer.id == 0x101)
             {
                 fprintf(fp, "  %02x", buffer.data[i]);
             }
         }
+
         //printf("\n");
-        if (buffer.id == id)
+        //if (buffer.id == 0x42F || buffer.id == 0x326)
+        if (buffer.id == id || buffer.id == 0x7E8 || buffer.id == 0x101)
         {
             fprintf(fp, "\n");
         }
@@ -222,15 +272,15 @@ void formatTRC(char* filename, char* fileOut, int id)
 int main(int argc, char* argv[])
 {
 #if defined DEVMODE
-    char filename[] = "rogue.trc";
-    char fileOut[] = "rogue.txt";
+    char filename[] = "longAFM.trc";
+    char fileOut[] = "longAFM_filtered.txt";
 #else 
     char filename[] = argv[1];
     char fileOut[] = argv[2];
 #endif
 
-    //formatTRC(filename, fileOut, 0x137);
-    formatTRC(filename, fileOut);
+    formatTRC(filename, fileOut, 0x7E0);
+    //formatTRC(filename, fileOut);
 
     printf("Task Complete\n");
 }
